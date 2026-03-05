26c9c82c47e17141994c770688b538582f52a207
vamsi-krishna2580 <216156130+vamsi-krishna2580@users.noreply.github.com>
2026-03-05 06:25:47 +0530

    news_scrapping

--- news_scrapping.py
+++ news_scrapping.py
@@ -1,25 +1,46 @@
+# import feedparser
+
+# def get_agri_news():
+
+#     url = "https://news.google.com/rss/search?q=agriculture+india"
+
+#     feed = feedparser.parse(url)
+
+#     news_list = []
+
+#     for entry in feed.entries[:5]:
+#         news_list.append(entry.title)
+
+#     return news_list
+
+# news = get_agri_news()
+
+# def prepare_news_summary(news):
+
+#     combined = "\n".join(news)
+
+#     return combined
+
+
+# print(prepare_news_summary(news))
+
+## Location Based
 import feedparser
 
-def get_agri_news():
+def get_local_agri_news(city):
 
-    url = "https://news.google.com/rss/search?q=agriculture+india"
+    query = f"agriculture OR farming OR crop {city}"
+
+    url = f"https://news.google.com/rss/search?q={query}&hl=en-IN&gl=IN&ceid=IN:en"
 
     feed = feedparser.parse(url)
 
-    news_list = []
+    news = []
 
     for entry in feed.entries[:5]:
-        news_list.append(entry.title)
+        news.append(entry.title)
 
-    return news_list
-
-news = get_agri_news()
-
-def prepare_news_summary(news):
-
-    combined = "\n".join(news)
-
-    return combined
+    return news
 
 
-print(prepare_news_summary(news))
+print(get_local_agri_news("Ongole"))
