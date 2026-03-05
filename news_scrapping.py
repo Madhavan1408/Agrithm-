aac89bb40663dcc30a11a861cd904b268298a944
vamsi-krishna2580 <216156130+vamsi-krishna2580@users.noreply.github.com>
2026-03-05 06:21:16 +0530

    news_scrapping

---
+++ news_scrapping.py
@@ -0,0 +1,25 @@
+import feedparser
+
+def get_agri_news():
+
+    url = "https://news.google.com/rss/search?q=agriculture+india"
+
+    feed = feedparser.parse(url)
+
+    news_list = []
+
+    for entry in feed.entries[:5]:
+        news_list.append(entry.title)
+
+    return news_list
+
+news = get_agri_news()
+
+def prepare_news_summary(news):
+
+    combined = "\n".join(news)
+
+    return combined
+
+
+print(prepare_news_summary())
